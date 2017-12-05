793bff993e4c53e14cc8c451eb06b9c63b5326de
Caio <Senac@47DE195352.senacrjedu.local>
2017-12-05 21:44:46 -0200

    Servlet Categoria e Fabricante

---
+++ ChipStore/src/br/com/chipstore/sevlet/IncluirCategoria.java
@@ -0,0 +1,71 @@
+package br.com.chipstore.sevlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.chipstore.model.Categoria;
+import br.com.chipstore.model.Fabricante;
+import br.com.chipstore.service.CategoriaService;
+import br.com.chipstore.service.FabricanteService;
+
+/**
+ * Servlet implementation class IncluirCategoria
+ */
+@WebServlet("/IncluirCategoria")
+public class IncluirCategoria extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IncluirCategoria() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter saida;
+		String nome;
+		
+		
+		nome = request.getParameter("nome");
+		
+		Categoria novaCategoria = new Categoria();
+		
+		novaCategoria.setNome(nome);
+		
+		
+		CategoriaService cs = new CategoriaService();
+		
+		long codigoGerado = cs.incluir(novaCategoria);
+		
+		if(codigoGerado > 0){
+			
+		} else{
+			
+		}
+		//request.setAttribute("mensagem", mensagem);
+		
+		//RequestDispatcher rd = request.getRequestDispatcher();
+		//rd.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ ChipStore/src/br/com/chipstore/sevlet/IncluirFabricante.java
@@ -0,0 +1,60 @@
+package br.com.chipstore.sevlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.chipstore.model.Fabricante;
+import br.com.chipstore.service.FabricanteService;
+
+
+@WebServlet("/IncluirFabricante")
+public class IncluirFabricante extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public IncluirFabricante() {
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter saida;
+		String nome;
+		
+		
+		nome = request.getParameter("nome");
+		
+		Fabricante novoFabricante = new Fabricante();
+		
+		novoFabricante.setNome(nome);
+		
+		
+		FabricanteService fs = new FabricanteService();
+		
+		long codigoGerado = fs.incluir(novoFabricante);
+		
+		if(codigoGerado > 0){
+			
+		} else{
+			
+		}
+		//request.setAttribute("mensagem", mensagem);
+		
+		//RequestDispatcher rd = request.getRequestDispatcher();
+		//rd.forward(request, response);
+		
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}

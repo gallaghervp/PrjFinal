59323ef866b00c4d6c7eaa45ff9ae2a450166de4
Vinicius <Vinicius@192.168.0.16>
2017-12-05 11:40:33 -0200

    Criação da ligação com o DB para Categoria e Produto.
    Atualização do DAOFactory e MySqlDAOFactory.
    
    Criação do Modelo para Produto e as páginas de testes.

---
+++ ChipStore/src/br/com/chipstore/dao/CategoriaDao.java
@@ -0,0 +1,14 @@
+package br.com.chipstore.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.chipstore.model.Categoria;
+
+public interface CategoriaDao {
+	public long incluir(Categoria categoria) throws SQLException;
+    public Categoria consultarPorCodigo(long codigo) throws SQLException;
+    public List<Categoria> listar() throws SQLException;
+    public boolean atualizar(Categoria categoria) throws SQLException;
+    public boolean remover(long codigo) throws SQLException;
+}
---
+++ ChipStore/src/br/com/chipstore/model/Produto.java
@@ -0,0 +1,72 @@
+package br.com.chipstore.model;
+
+public class Produto {
+	
+	private long codigoBarras;
+	private String nome;
+	private Categoria categoria;
+	private double preco;
+	private boolean disponivel;
+	private String imagem;
+	private long quantidade;
+	
+	public Produto() {
+		super();
+	}
+	public long getCodigoBarras() {
+		return codigoBarras;
+	}
+	public void setCodigoBarras(long codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public boolean isDisponivel() {
+		return disponivel;
+	}
+	public void setDisponivel(boolean disponivel) {
+		this.disponivel = disponivel;
+	}
+	public String getImagem() {
+		return imagem;
+	}
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+	public long getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(long quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	@Override
+	public String toString() {
+		return "Produto [codigoBarras=" + codigoBarras + ", nome=" + nome + ", categoria=" + categoria + ", "
+				+ "preco=" + preco + ", disponivel=" + disponivel + ", imagem=" + imagem + ", quantidade=" + quantidade + "]";
+	}
+	
+	
+	
+
+
+}

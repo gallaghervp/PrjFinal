125a280c19f61eada8bacdc9c00fa9d55e5f7dd0
Gabrielpbs <g_batistas@yahoo.com.br>
2017-12-06 13:51:16 -0200

    Update bancodedados.sql

--- ChipStore/documentacao/mysql/bancodedados.sql
+++ ChipStore/documentacao/mysql/bancodedados.sql
@@ -18,16 +18,19 @@ CREATE TABLE clientes (
     endereco VARCHAR(20) NOT NULL,
 	complemento VARCHAR (10) NOT NULL,
     bairro VARCHAR(20) NOT NULL,
-    municipio VARCHAR(20) NOT NULL
+    municipio VARCHAR(20) NOT NULL,
+	senha VARCHAR(20) NOT NULL
 );
 
 
 /* CRIAÇÃO DA TABELA DOS PRODUTOS CADASTRADOS. O CÓDIGO SERÁ COLOCADO MANUALMENTE, SE POSSÍVEL,
  JÁ QUE NAO DEU PARA COLOCAR AUTO_INCREMENT */
 CREATE TABLE produtos (
+	id INT (20) NOT NULL,
     codigobarra INT(20) NOT NULL,
     nome VARCHAR(10) NOT NULL,
-    categoria VARCHAR(10) NOT NULL,
+    modelo VARCHAR(10) NOT NULL,
+	descricao VARCHAR(20) NOT NULL
     preco INT(10) NOT NULL,
     disponivel BOOLEAN NOT NULL,
     imagem VARCHAR(10) NOT NULL,
@@ -48,7 +51,8 @@ CREATE TABLE administrador (
     estado VARCHAR(20) NOT NULL,
     endereco VARCHAR(20) NOT NULL,
     bairro VARCHAR(20) NOT NULL,
-    municipio VARCHAR(20) NOT NULL
+    municipio VARCHAR(20) NOT NULL,
+	senha VARCHAR(20) NOT NULL
 );
 
 /* tabela do tipo de categoria que os produtos ficarão armazenados: roupas, tenis, etc */

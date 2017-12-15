3799d79fa585577f6c2bfedcd0731a83c36a5eb4
Julio <Senac@47DE195344.senacrjedu.local>
2017-12-15 21:21:06 -0200

    Atualização de páginas JSP!

---
+++ ChipStore/WebContent/js/login.js
@@ -0,0 +1,73 @@
+/* LOGIN */
+
+function cambiar_login() {
+  document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";  
+document.querySelector('.cont_form_login').style.display = "block";
+document.querySelector('.cont_form_sign_up').style.opacity = "0";               
+
+setTimeout(function(){  document.querySelector('.cont_form_login').style.opacity = "1"; },400);  
+  
+setTimeout(function(){    
+document.querySelector('.cont_form_sign_up').style.display = "none";
+},200);  
+  }
+
+function cambiar_sign_up(at) {
+  document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
+  document.querySelector('.cont_form_sign_up').style.display = "block";
+document.querySelector('.cont_form_login').style.opacity = "0";
+  
+setTimeout(function(){  document.querySelector('.cont_form_sign_up').style.opacity = "1";
+},100);  
+
+setTimeout(function(){   document.querySelector('.cont_form_login').style.display = "none";
+},400);  
+
+
+}    
+    
+function ocultar_login_sign_up() {
+
+document.querySelector('.cont_forms').className = "cont_forms";  
+document.querySelector('.cont_form_sign_up').style.opacity = "0";               
+document.querySelector('.cont_form_login').style.opacity = "0"; 
+
+setTimeout(function(){
+document.querySelector('.cont_form_sign_up').style.display = "none";
+document.querySelector('.cont_form_login').style.display = "none";
+},500);  
+  
+  }
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.10&appId=172816859943992";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+/* EFEITO DESLIZANTE */
+
+jQuery(document).ready(function ($) {
+    $(".scroll").click(function (event) {
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 800);
+    });
+});
+
+/* OPCAO DE LOGIN */
+
+function verificaPessoa() {
+    if (document.getElementById('tipoPessoa').value == 'PF') {
+        document.getElementById('pessoaFisica').style.display = 'block';
+        document.getElementById('pessoaJuridica').style.display = 'none';
+    } else {
+        document.getElementById('pessoaFisica').style.display = 'none';
+        document.getElementById('pessoaJuridica').style.display = 'block';
+    }
+}
+
---
+++ ChipStore/WebContent/template/sobre.jsp
@@ -0,0 +1,40 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+  <!--------------- SOBRE -------------------->
+
+    <div id="sobre" class="heading-index">
+        <h1 style="text-shadow: 0.1em 0.1em 0.2em #87F">Sobre a ChipStore</h1>
+    </div>
+
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-default" style="border-color: #847BF6;box-shadow: 6px 6px 10px #847BF6;">
+                    <div class="panel-body" style="color: #000000">
+                        <p>O nome do nosso site vem de Application Program Interface, que é um tipo de programa utilizado para fazer com que dois outros programas diferentes troquem informações entre si. Um programa do tipo API pode ser entendido como uma interface.</p>
+
+                        <p>Escolhemos este nome pois além de ser um jargão muito conhecido na área de informática, reflete também o nosso papel, que é facilitar o contato entre as empresas e os profissionais de informática.</p>
+
+                        <p>Não somos uma associação de profissionais e não estamos ligados a nenhum sindicato ou entidades de classe. Nosso compromisso é atender a comunidades de profissionais de informática, possibilitando a troca de informações e experiências.</p>
+
+                        <p>ChipStore é uma marca registrada no INPI, todas as informações deste site são protegidas pela lei de direitos autorais nro 9.610/98, não podendo ser copiadas, utilizadas por outros sites, ou disponibilizadas em outro local. Caso saiba de alguma violação denuncie.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="parallax-container" id="parallaxIndex" data-speed="6" data-type="background"></div>
+    <div id="contato" class="heading-index">
+    </div>
+
+</body>
+</html>
